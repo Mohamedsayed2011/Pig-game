ff18140dda86f2b8763996d1de384ad54c2281b4
mohamedsayed2011 <mohamed.sayed8899@gmail.com>
2019-06-19 00:48:23 +0200

    logic is done!

--- 4-DOM-Pig-Game/app.js
+++ 4-DOM-Pig-Game/app.js
@@ -8,3 +8,100 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var  scores,activePlaye,currentScore,check ;
+
+
+
+newgame();
+
+document.querySelector('.btn-roll').addEventListener('click',function(){
+
+var dice=Math.floor(Math.random()*6)+1;
+var pic=document.querySelector('.dice');
+pic.style.display='block';
+pic.src='dice-'+dice+'.png';
+currentScore=currentScore+dice;
+if (dice!=1)
+document.getElementById('current-' +activePlayer).textContent=currentScore;
+ else
+ {
+    nextPlayer();
+ }  
+ if (check==true)
+ {
+     alert ("Player "+ (activePlayer+1)+ "  " +"won");
+     newgame();
+ }
+
+})
+
+///////////////////////////////////////
+document.querySelector('.btn-hold').addEventListener('click',function(){
+if(!check)
+{
+    //add current score
+scores[activePlayer]+=currentScore; 
+
+document.querySelector('#score-'+activePlayer).textContent=scores[activePlayer];
+if (scores[activePlayer]>=20)
+{
+  
+    document.querySelector('#name-'+activePlayer).textContent="Winner!";
+    document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+   
+   check=true;
+   
+}
+
+else
+{
+    nextPlayer();
+}
+
+}
+
+
+
+})
+//////////////////////////////////////////////////
+document.querySelector('.btn-new').addEventListener('click',function(){
+newgame();
+
+
+
+
+})
+
+////////////////////////////////////////////////////////
+function nextPlayer(){
+    
+    activePlayer === 0 ? activePlayer=1 :activePlayer=0;
+    currentScore=0;
+    document.querySelector('#current-0').textContent=0;
+    document.querySelector('#current-1').textContent=0; 
+    document.querySelector('.dice').style.display='none';
+    document.querySelector(".player-0-panel").classList.toggle('active');
+    document.querySelector(".player-1-panel").classList.toggle('active');
+}
+
+function newgame()
+{
+   scores=[0,0];
+   activePlayer=0;
+  currentScore=0;
+  check =false;
+  document.querySelector(".player-0-panel").classList.add('active');
+  document.querySelector(".player-1-panel").classList.remove('active');
+  document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+    document.getElementById('name-0').textContent='Player 1';
+    document.getElementById('name-1').textContent='Player 2';
+    document.querySelector('.dice').style.display='none';
+    document.querySelector('#score-0').textContent=0;
+    document.querySelector('#score-1').textContent=0;
+    document.querySelector('#current-0').textContent=0;
+    document.querySelector('#current-1').textContent=0;
+    
+}
+
